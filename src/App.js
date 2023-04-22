18e4d355f0331723a877debceea1f83e254224cb
samofoke <sabataernestmofokeng@gmail.com>
2023-04-22 11:58:56 +0200

    configuring the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This website is under Development.
-        </p>
+      <header className="">
+        <p>This website is under Development.</p>
       </header>
     </div>
   );
